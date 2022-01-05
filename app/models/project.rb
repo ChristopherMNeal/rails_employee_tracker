0b3799ae66b093672e2fc7cbbb9af8e0d85d655a
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-05 15:40:11 -0800

    add database seeds, WIP: add views
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
   has_many :employee_projects
   has_many :employees, :through => :employee_projects
-  scope :name, -> (name_parameter) { where(name: name_parameter) } 
+  scope :search, -> (name_parameter) { where(name: name_parameter) } 
 end
