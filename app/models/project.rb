471ec53b3773fd7382bac4ecf378d71661e06084
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-06 11:18:20 -0800

    add titelize method to project.rb
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,11 @@ class Project < ApplicationRecord
   has_many :employee_projects
   has_many :employees, :through => :employee_projects
   scope :search, -> (name_parameter) { where(name: name_parameter) } 
+
+  before_save(:titleize_project)
+
+  private
+    def titleize_project
+      self.name = self.name.titleize
+    end
 end
